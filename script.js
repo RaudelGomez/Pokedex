4fef43281eb3e4a0f336e0ad43076194fed53d4b
Raudel Gomez Smith <gomezsmith.raudel@gmail.com>
2024-05-29 15:06:55 +0200

    templates script created

--- script.js
+++ script.js
@@ -62,21 +62,7 @@ async function loadingPokemons() {
 function LoadingOnePokemon(pokemonData, getPokemonColorPhoto) {
 	const pokemonList = document.getElementById("pokemonList");
 	pokemonList.innerHTML += /*html*/ `
-    <div id="card${pokemonData.id}" class="card" style="width: 15rem; background-color: ${getPokemonColorPhoto[0]["color"]}">
-      <h5 class="card-title p-2 text-center position-relative">
-        <span class="pokemonId-card position-absolute">#${pokemonData.id}</span>
-        <span class="first-letter-uppercase">${pokemonData.name}</span>
-      </h5>
-      <div class="container-img-card"></div>
-      <img
-        src="${pokemonData.sprites.other.dream_world.front_default}"
-        class="card-img-top img-pokemon"
-        alt="${pokemonData.name}"
-      />
-      <div id="types-pokemon${pokemonData.id}"
-        class="pokemon-types card-body d-flex justify-content-center align-items-center gap-3"
-      ></div>
-    </div>
+    ${LoadingOnePokemonHTML(pokemonData, getPokemonColorPhoto)}
   `;
 }
 
@@ -108,14 +94,7 @@ function renderTypesPokemon(idContainer, getPokemonColorPhoto, pokemonData) {
 	for (let i = 0; i < getPokemonColorPhoto.length; i++) {
 		const type = getPokemonColorPhoto[i];
 		typesPokemonContainer.innerHTML += /*html*/ `
-    <figure class="d-flex justify-content-center flex-column align-items-center mb-0" onclick="changeBGColor('${type.color}', ${pokemonData.id})">
-      <p id="helpColor${pokemonData.id}${i}" class="d-none position-absolute help-color text-center rounded-1 p-1">Click here<br>to change color</p>
-      <img class="type-pokemon-img" src="${type.img}" alt="${type.name}" 
-      onmouseover="displayBlock('${pokemonData.id}', '${i}', '${pokemonData.types.length}')" onmouseout="displayNone('${pokemonData.id}', '${i}')">  
-      <figcaption>
-        ${type.name}
-      </figcaption>
-    </figure>
+      ${typesPokemonContainerHTML(pokemonData, type, i)}
     `;
 	}
 }
---
+++ templates.js
@@ -0,0 +1,32 @@
+function LoadingOnePokemonHTML(pokemonData, getPokemonColorPhoto) {
+	return /*html*/ `
+    <div id="card${pokemonData.id}" class="card" style="width: 15rem; background-color: ${getPokemonColorPhoto[0]["color"]}">
+      <h5 class="card-title p-2 text-center position-relative">
+        <span class="pokemonId-card position-absolute">#${pokemonData.id}</span>
+        <span class="first-letter-uppercase">${pokemonData.name}</span>
+      </h5>
+      <div class="container-img-card"></div>
+      <img
+        src="${pokemonData.sprites.other.dream_world.front_default}"
+        class="card-img-top img-pokemon"
+        alt="${pokemonData.name}"
+      />
+      <div id="types-pokemon${pokemonData.id}"
+        class="pokemon-types card-body d-flex justify-content-center align-items-center gap-3"
+      ></div>
+    </div>
+`;
+}
+
+function typesPokemonContainerHTML(pokemonData, type, i) {
+	return /*html*/ `
+    <figure class="d-flex justify-content-center flex-column align-items-center mb-0" onclick="changeBGColor('${type.color}', ${pokemonData.id})">
+      <p id="helpColor${pokemonData.id}${i}" class="d-none position-absolute help-color text-center rounded-1 p-1">Click here<br>to change color</p>
+      <img class="type-pokemon-img" src="${type.img}" alt="${type.name}" 
+      onmouseover="displayBlock('${pokemonData.id}', '${i}', '${pokemonData.types.length}')" onmouseout="displayNone('${pokemonData.id}', '${i}')">  
+      <figcaption>
+        ${type.name}
+      </figcaption>
+    </figure>
+    `;
+}
