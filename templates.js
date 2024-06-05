26d4e82df22109b5d1d555b30716ea4bc1659bad
Raudel Gomez Smith <gomezsmith.raudel@gmail.com>
2024-06-05 18:17:27 +0200

    stop propagation out and final Project

--- templates.js
+++ templates.js
@@ -33,9 +33,7 @@ function openImgHTML(pokemonData, color) {
 	return /*html*/ `
     <div class="arrow-container" onclick="stopPropagation(event)">
       <svg id="arrow-left" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-arrow-left-circle-fill arrow-slide" viewBox="0 0 16 16">
-        <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z" onclick="stopPropagation(event); beforePokemon(${
-					pokemonData.id
-				})"/>
+        <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z" onclick="beforePokemon(${pokemonData.id})"/>
       </svg>
     </div>
     <div id="cardOpen${pokemonData.id}" class="card" style="width: 15rem; background-color: ${color}; padding: 0.09rem;" onclick="stopPropagation(event)">
@@ -55,7 +53,7 @@ function openImgHTML(pokemonData, color) {
       <div id="info-pokemon-open"></div>
     </div>
     <div class="arrow-container" onclick="stopPropagation(event)">
-      <svg id="arrow-right" xmlns="http://www.w3.org/2000/svg"fill="currentColor" class="bi bi-arrow-right-circle-fill arrow-slide" viewBox="0 0 16 16"  onclick="stopPropagation(event); nextPokemon(${pokemonData.id})">
+      <svg id="arrow-right" xmlns="http://www.w3.org/2000/svg"fill="currentColor" class="bi bi-arrow-right-circle-fill arrow-slide" viewBox="0 0 16 16" onclick="nextPokemon(${pokemonData.id})">
         <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
       </svg>
     </div>
